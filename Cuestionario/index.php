bd9049ac110b14693dad3b1a96cdbeb28eef4888
Javier Morilla Barrena <jmorillabarrena@gmail.com>
2025-10-29 21:05:07 +0100

    Cuestionario

---
+++ Cuestionario/index.php
@@ -0,0 +1,26 @@
+<?php
+    include_once('datos.php');
+    include_once('funciones.php');
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cuestionario</title>
+    <link rel="stylesheet" href="css/estilos.css">
+</head>
+<body>
+    <h1>Cuestionario Tibiano</h1>
+    <?php
+        //comprobar si ya se ha contestado al formulario
+        if(count($_POST) > 1) {
+            mostrar_resultado($preguntas);
+        }else{
+            mostrar_cuestionario($preguntas);
+        }
+    ?>
+</body>
+    <script src="js/tibia-quiz.js"></script>
+</html>
