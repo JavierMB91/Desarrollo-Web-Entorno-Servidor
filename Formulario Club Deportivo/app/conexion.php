f896d2897cdae3ec5f894bf19364848bbed9626f
Javier Morilla Barrena <jmorillabarrena@gmail.com>
2026-01-12 18:11:04 +0100

    conexion

--- Formulario Club Deportivo/app/conexion.php
+++ Formulario Club Deportivo/app/conexion.php
@@ -1,13 +1,13 @@
 <?php
-$host = getenv("DB_HOST");
-$db   = getenv("DB_NAME");
-$user = getenv("DB_USER");
-$pass = getenv("DB_PASSWORD");
+// $host = getenv("DB_HOST");
+// $db   = getenv("DB_NAME");
+// $user = getenv("DB_USER");
+// $pass = getenv("DB_PASSWORD");
 
-// $host = ("sql100.infinityfree.com");
-// $db   = ("if0_40888308_mi_aplicacion");
-// $user = ("if0_40888308");
-// $pass = ("M5tkkw8ww");
+$host = ("sql100.infinityfree.com");
+$db   = ("if0_40888308_mi_aplicacion");
+$user = ("if0_40888308");
+$pass = ("M5tkkw8ww");
 
 try {
     $pdo = new PDO(
